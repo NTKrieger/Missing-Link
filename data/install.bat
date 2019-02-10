77eae90c0eb7042776a9b6e15b3c67e7edb2755d
OmegaNix <N.T.KRIEGER@GMAIL.COM>
2019-02-10 16:39:21 -0500

    working on installer for Windows

--- data/install.bat
+++ data/install.bat
@@ -1,6 +1,6 @@
 echo off
 title Missing Link Installer
-::echo Try to install NodeJS
-::MSIEXEC /I ".\node-v10.15.1-x64.msi" 
+echo Installing NodeJS throws an error message from msiexec...
+MSIEXEC /I ".\node-v10.15.1-x64.msi" :
+echo Installing SQLite works fine but I don't want a new window....
 start sqlite3.exe
-
